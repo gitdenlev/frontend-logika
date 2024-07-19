bfdf09fa0e79c698ebef226198973d53b6923675
Denys <levchenkoden02@gmail.com>
2024-07-19 08:13:59 +0300

    Init rep

---
+++ script.js
@@ -0,0 +1,66 @@
+// ------------------------------- Робота з рядками -------------------------------
+
+// Додавання рядків
+console.log("Hello" + "World");
+console.log("Hello" + " " + "World");
+
+// Додавання чисел в рядки
+console.log(2024 + " " + "рік");
+
+// Унарний плюс
+console.log(+"327");
+
+// Інтерполяція
+const city = "Kyiv";
+console.log(`Привіт, ${city}!`);
+
+// Отримання символу
+const carName = "Mercedes";
+console.log(carName[0]);
+
+// Отримання підрядка
+let friends = "Василь;Артем;Марія;Дарина";
+console.log(friends.slice(7, 13));
+
+// Заміна символів
+friends = "Ва си ль;Ар тем;Ма рія;Да ри на";
+console.log(friends.replaceAll(" ", ""));
+
+// Перевірка підрядка
+friends = "Василь;Артем;Марія;Дарина";
+
+if (friends.includes("Марія")) {
+  // повертається true якщо рядок містить 'Марія'
+  console.log("Марія є ✅");
+} else {
+  console.log("Марії немає ❌");
+}
+
+// Об'єднання в рядок
+friends = ["Василь", "Артем", "Марія", "Дарина"];
+console.log(friends.join(";")); // Отримаємо рядок "Василь;Артем;Марія;Дарина"
+
+// ------------------------------- Робота з cookie -------------------------------
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+// Установить значение поля ввода из cookie при загрузке страницы
+window.onload = function () {
+  const inputField = document.getElementById("inputField");
+  const savedName = getCookie("name");
+  if (savedName) {
+    inputField.value = savedName;
+  }
+};
+
+// Записать значение в cookie при нажатии на кнопку
+document.getElementById("saveButton").onclick = function () {
+  const inputField = document.getElementById("inputField");
+  const userName = inputField.value;
+  document.cookie = `name=${userName};max-age=86400;`;
+  console.log(document.cookie);
+};
